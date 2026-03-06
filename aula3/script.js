fddf536b7de0f52285675927c90b4f066abcf6f0
apolo <apoloprog@gmail.com>
2026-03-06 08:50:20 -0300

    sl

---
+++ aula3/script.js
@@ -0,0 +1,10 @@
+function soma(){
+    const valorinput=document.getElementById('cpf1').value;
+    const valorendereco =document.getElementById('endereco1').value;
+    const valoremail =document.getElementById('email1').value;
+    const valornome =document.getElementById('nome1').value;
+    
+    alert("ola " +valornome+" seu cpf e: "+valorinput+" seu email é "+valoremail+" e o seu endereço é "+valorendereco);
+
+        
+}
